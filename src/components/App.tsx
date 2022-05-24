d8e52a571db419837af9ddbd4066bcfc8d76811f
Garfield550 <openpgp@550.moe>
2022-05-24 14:40:52 +0000

    feat: add 3rd components demo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import Avatar from 'components/Avatar'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 
 function App() {
   return (
@@ -19,6 +20,20 @@ function App() {
             />
           </p>
         </div>
+      </div>
+      <div className="py-16 px-4 mx-auto ">
+        <Tabs>
+          <TabList>
+            <Tab>Title 1</Tab>
+            <Tab>Title 2</Tab>
+          </TabList>
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
   )
